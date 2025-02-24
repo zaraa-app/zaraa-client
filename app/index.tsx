121b732cf4e3d02062aa1ca6eae0f7f834abd25a
yznki <yznkiswani@gmail.com>
2025-02-24 22:12:16 +0100

    (ZARAA-17) Add initial constants, components, and layout for the app. Added the splashcreen and icon.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { Image, ScrollView, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import Images from "@/constants/Images";
+import TextContent from "@/components/TextContent";
+import HeadingContent from "@/components/HeadingContent";
+
 export default function Index() {
   return (
-    <View className="flex h-svh w-svw flex-1 items-center justify-center">
-      <Text className="font-bold">Tesdwakjdwaudaiudhwauiduait</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="flex h-full items-center justify-center px-8">
+          <Image source={Images.mascot} />
+          <HeadingContent size="h5">Let's Get Growing! 🌱</HeadingContent>
+          <TextContent className="text-lg">Discover the joy of nurturing plants, one leaf at a time.</TextContent>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
