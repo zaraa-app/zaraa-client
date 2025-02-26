d3416b4a37364924f8a7619b1ef4f4d866f2b754
yznki <yznkiswani@gmail.com>
2025-02-26 21:33:34 +0100

    (ZARAA-18) I created the sign up logic, the select preferences and final set page is yet to come, to be done with onboarding logic.

--- app/(auth)/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const SplashScreen = () => {
-  return <></>;
-};
-export default SplashScreen;
--- components/HeadingContent.tsx
+++ components/HeadingContent.tsx
@@ -1,40 +1,46 @@
 import { Text } from "react-native";
 import React from "react";
+import normalize from "../utils/normalize"; // Import the normalize function
 
 interface HeadingContentProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   size: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "h7";
   className?: string;
+  heading?: string;
 }
 
-const HeadingContent = ({ children, size, className }: HeadingContentProps) => {
-  let headingClasses = "font-inter w-full";
+const HeadingContent = ({ children, size, className, heading: title }: HeadingContentProps) => {
+  let textStyle = {};
 
   switch (size) {
     case "h1":
-      headingClasses += " text-7xl font-black leading-[87px]";
+      textStyle = { fontSize: normalize(58), lineHeight: normalize(87), fontWeight: "900" };
       break;
     case "h2":
-      headingClasses += " text-6xl font-black leading-[72px]";
+      textStyle = { fontSize: normalize(48), lineHeight: normalize(72), fontWeight: "900" };
       break;
     case "h3":
-      headingClasses += " text-5xl font-black leading-[60px]";
+      textStyle = { fontSize: normalize(40), lineHeight: normalize(60), fontWeight: "900" };
       break;
     case "h4":
-      headingClasses += " text-4xl font-black leading-[51px]";
+      textStyle = { fontSize: normalize(34), lineHeight: normalize(51), fontWeight: "900" };
       break;
     case "h5":
-      headingClasses += " text-3xl font-black leading-[42px]";
+      textStyle = { fontSize: normalize(28), lineHeight: normalize(42), fontWeight: "900" };
       break;
     case "h6":
-      headingClasses += " text-2xl font-black leading-[36px]";
+      textStyle = { fontSize: normalize(24), lineHeight: normalize(36), fontWeight: "900" };
       break;
     case "h7":
-      headingClasses += " text-xl font-black leading-[30px]";
+      textStyle = { fontSize: normalize(20), lineHeight: normalize(30), fontWeight: "900" };
       break;
   }
 
-  return <Text className={`${headingClasses} ${className}`}>{children}</Text>;
+  return (
+    <Text style={[textStyle]} className={`w-full font-inter ${className}`} adjustsFontSizeToFit>
+      {children || title}
+    </Text>
+  );
 };
 
 export default HeadingContent;
